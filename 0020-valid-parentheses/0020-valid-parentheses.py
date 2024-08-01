5d0b6e9bb278f06daf36d2b6106177caa2db19c3
Xun Yang <87108033+xunyang03@users.noreply.github.com>
2024-08-02 01:47:53 +0800

    Time: 11 ms (83.41%), Space: 11.9 MB (18.1%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for c in s:
+            if c == '(':
+                stack.append(')')
+            elif c == '[':
+                stack.append(']')
+            elif c == '{':
+                stack.append('}')
+            elif not stack or c != stack[-1]:
+                return False
+            else:
+                stack.pop()
+        return True if not stack else False
