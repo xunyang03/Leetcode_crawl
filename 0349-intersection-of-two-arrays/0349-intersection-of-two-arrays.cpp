dccbdaa5cb54a604a30b9e19d9053eaefe2e6888
Xun Yang <1019805418@qq.com>
2024-08-17 23:19:01 +0800

    Time: 9 ms (18.43%), Space: 14 MB (22.72%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> result;
+        unordered_set<int> num1_set(nums1.begin(), nums1.end());
+        for (auto& num : nums2){
+            if (num1_set.find(num) != num1_set.end())
+                result.insert(num);
+        }
+        return vector<int>(result.begin(),result.end());
+    }
+};
